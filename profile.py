8cd8e3ce6674f64cbeced445048c4482bcd6b35d
Michael Zink <zink@ecs.umass.edu>
2025-02-22 10:35:18 -0500

    Update profile.py to allow Ubuntu 20

--- profile.py
+++ profile.py
@@ -31,7 +31,7 @@ clusters = [
 
 # Pick your image.
 imageList = [
-    #('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04'),
+    ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU20-64-STD', 'UBUNTU 20.04'),
     ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD', 'UBUNTU 18.04'),
     ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU16-64-STD', 'UBUNTU 16.04'),
     #('urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS8-64-STD', 'CENTOS 8.4'),
