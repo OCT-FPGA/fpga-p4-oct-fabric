97fc50b1a8163e7c8060281a19f38a1d0f138547
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2025-02-22 23:36:52 -0500

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -1,86 +1,111 @@
 #!/usr/bin/env bash
-#
-# (C) Copyright 2019, Xilinx, Inc.
-#
-#!/usr/bin/env bash
 
 install_xrt() {
-    echo "Download XRT installation package"
-    wget -cO - "https://www.xilinx.com/bin/public/openDownload?filename=$XRT_PACKAGE" > /tmp/$XRT_PACKAGE
-    
     echo "Install XRT"
-    if [[ "$OSVERSION" == "ubuntu-16.04" ]] || [[ "$OSVERSION" == "ubuntu-18.04" ]] || [[ "$OSVERSION" == "ubuntu-20.04" ]]; then
+    if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
         echo "Ubuntu XRT install"
         echo "Installing XRT dependencies..."
         apt update
         echo "Installing XRT package..."
-        apt install -y /tmp/$XRT_PACKAGE
-    elif [[ "$OSVERSION" == "centos-7" ]] ; then
-        echo "CentOS 7 XRT install"
-        echo "Installing XRT dependencies..."
-        yum install -y epel-release
-        echo "Installing XRT package..."
-        yum install -y /tmp/$XRT_PACKAGE
-    elif [[ "$OSVERSION" == "centos-8" ]]; then
-        echo "CentOS 8 XRT install"
-        echo "Installing XRT dependencies..."
-        #sudo yum remove -y xrt
-        yum config-manager --set-enabled powertools
-        yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-        yum config-manager --set-enabled appstream
-        echo "Installing XRT package..."
-        sudo yum install -y /tmp/$XRT_PACKAGE
+        apt install -y $XRT_BASE_PATH/$TOOLVERSION/$OSVERSION/$XRT_PACKAGE
     fi
-    #rm /tmp/$XRT_PACKAGE
+    sudo bash -c "echo 'source /opt/xilinx/xrt/setup.sh' >> /etc/profile"
+    sudo bash -c "echo 'source $VITIS_BASE_PATH/$VITISVERSION/settings64.sh' >> /etc/profile"
 }
 
 check_xrt() {
-    if [[ "$OSVERSION" == "ubuntu-16.04" ]] || [[ "$OSVERSION" == "ubuntu-18.04" ]] || [[ "$OSVERSION" == "ubuntu-20.04" ]]; then
+    if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
         XRT_INSTALL_INFO=`apt list --installed 2>/dev/null | grep "xrt" | grep "$XRT_VERSION"`
-    elif [[ "$OSVERSION" == "centos-7" ]] || [[ "$OSVERSION" == "centos-8" ]]; then
-        XRT_INSTALL_INFO=`yum list installed 2>/dev/null | grep "xrt" | grep "$XRT_VERSION"`
+    else
+        echo "Unsupported OS: $OSVERSION"
+        exit 1 
     fi
 }
 
 install_xbflash() {
-    cp -r /proj/oct-fpga-p4-PG0/tools/xbflash/${OSVERSION} /tmp
+    cp -r $XBFLASH_BASE_PATH/${OSVERSION} /tmp
     echo "Installing xbflash."
-    if [[ "$OSVERSION" == "ubuntu-18.04" ]] || [[ "$OSVERSION" == "ubuntu-20.04" ]]; then
+    if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
         apt install /tmp/${OSVERSION}/*.deb
-    elif [[ "$OSVERSION" == "centos-7" ]] || [[ "$OSVERSION" == "centos-8" ]]; then
-        yum install /tmp/${OSVERSION}/*.rpm
-    fi    
+    else
+        echo "Unsupported OS: $OSVERSION"
+        exit 1 
+    fi 
 }
 
-verify_install() {
-    errors=0
-    check_xrt
-    if [ $? == 0 ] ; then
-        echo "XRT installation verified."
-    else
-        echo "XRT installation could not be verified."
-        errors=$((errors+1))
+detect_cards() {
+    lspci > /dev/null
+    if [ $? != 0 ] ; then
+        if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
+            apt-get install -y pciutils
+        elif [[ "$OSVERSION" == "centos-7" ]] || [[ "$OSVERSION" == "centos-8" ]]; then
+            yum install -y pciutils
+        fi
     fi
-    return $errors
+    if [[ "$OSVERSION" == "ubuntu-20.04" ]] || [[ "$OSVERSION" == "ubuntu-22.04" ]]; then
+        PCI_ADDR=$(lspci -d 10ee: | awk '{print $1}' | head -n 1)
+        if [ -n "$PCI_ADDR" ]; then
+            U280=$((U280 + 1))
+        else
+            echo "Error: No card detected."
+            exit 1
+        fi
+    fi
 }
-SHELL=1
+
+install_config_fpga() {
+    echo "Installing config-fpga."
+    cp $CONFIG_FPGA_PATH/* /usr/local/bin
+}
+
+disable_pcie_fatal_error() {
+    echo "Disabling PCIe fatal error reporting for node: $NODE_ID"
+    sudo /proj/octfpga-PG0/tools/pcie_disable_fatal.sh $PCI_ADDR
+}
+
+XRT_BASE_PATH="/proj/octfpga-PG0/tools/deployment/xrt"
+SHELL_BASE_PATH="/proj/octfpga-PG0/tools/deployment/shell"
+XBFLASH_BASE_PATH="/proj/octfpga-PG0/tools/xbflash"
+VITIS_BASE_PATH="/proj/octfpga-PG0/tools/Xilinx/Vitis"
+CONFIG_FPGA_PATH="/proj/octfpga-PG0/tools/post-boot"
+
 OSVERSION=`grep '^ID=' /etc/os-release | awk -F= '{print $2}'`
 OSVERSION=`echo $OSVERSION | tr -d '"'`
 VERSION_ID=`grep '^VERSION_ID=' /etc/os-release | awk -F= '{print $2}'`
 VERSION_ID=`echo $VERSION_ID | tr -d '"'`
 OSVERSION="$OSVERSION-$VERSION_ID"
-TOOLVERSION=$1
+WORKFLOW=$1
+TOOLVERSION=$2
+VITISVERSION="2023.1"
 SCRIPT_PATH=/local/repository
 COMB="${TOOLVERSION}_${OSVERSION}"
 XRT_PACKAGE=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $1}' | awk -F= '{print $2}'`
+SHELL_PACKAGE=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $2}' | awk -F= '{print $2}'`
+DSA=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $3}' | awk -F= '{print $2}'`
+PACKAGE_NAME=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $5}' | awk -F= '{print $2}'`
+PACKAGE_VERSION=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $6}' | awk -F= '{print $2}'`
 XRT_VERSION=`grep ^$COMB: $SCRIPT_PATH/spec.txt | awk -F':' '{print $2}' | awk -F';' '{print $7}' | awk -F= '{print $2}'`
+FACTORY_SHELL="xilinx_u280_GOLDEN_8"
+NODE_ID=$(hostname | cut -d'.' -f1)
+#PCI_ADDR=$(lspci -d 10ee: | awk '{print $1}' | head -n 1)
 
-install_xrt
-install_xbflash
-verify_install
-if [ $? == 0 ] ; then
-    echo "XRT and shell package installation successful."
+detect_cards
+check_xrt
+if [ $? == 0 ]; then
+    echo "XRT is already installed."
 else
-    echo "XRT installation failed."
-    exit 1
+    echo "XRT is not installed. Attempting to install XRT..."
+    install_xrt
+
+    check_xrt
+    if [ $? == 0 ]; then
+        echo "XRT was successfully installed."
+    else
+        echo "Error: XRT installation failed."
+        exit 1
+    fi
 fi
+# Disable PCIe fatal error reporting
+disable_pcie_fatal_error 
+install_config_fpga
+install_xbflash
